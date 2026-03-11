e02e7657c6da4297ee5db62a9766c05bec62db2a
Pallavi Bakale <pallavibakale09@gmail.com>
2026-03-11 11:10:05 -0400

    TD13

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-p = 1000;
-r = 0.05;
-n = 4;
-t = 3;
-let a = p * (1 + r / n) ** (n * t);
-console.log(`The compound interest after ${t} years is: ${a}`);
+num1 = 22;
+num2 = 33;
+num3 = 44;
+num4 = 55;
+
+if (num1 % 2 == 0) {
+  console.log(num1 + " is even");
+} else {
+  console.log(num1 + " is odd");
+}
+if (num2 % 2 == 0) {
+  console.log(num2 + " is even");
+} else {
+  console.log(num2 + " is odd");
+}
+if (num3 % 2 == 0) {
+  console.log(num3 + " is even");
+} else {
+  console.log(num3 + " is odd");
+}
+if (num4 % 2 == 0) {
+  console.log(num4 + " is even");
+} else {
+  console.log(num4 + " is odd");
+}
