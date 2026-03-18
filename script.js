8c87c3b86b1290bb69f55cf80782cd0b074f7e6e
Pallavi Bakale <pallavibakale09@gmail.com>
2026-03-17 23:03:11 -0400

    TD14:js array

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ arr1 = [
   5, 5, 8, 10, 679, 78, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
 ];
 
-console.log("greatest number in arr1:", arr1.sort((a, b) => b - a)[0]);
+function compareDescending(a, b) {
+  return b - a;
+}
+
+console.log("greatest number in arr1:", arr1.sort(compareDescending)[0]);
+
+arr2 = [12, 4, 5, 6];
 sum = 0;
-arr1.forEach((element) => {
+arr2.forEach((element) => {
   sum += element;
 });
 
-console.log("sum of all elements in arr1:", sum);
+console.log("sum of all elements in arr2:", sum);
 
-let oddNumbers = arr1.filter((element) => element % 2 !== 0);
+arr3 = [1, 2, 3, 4, 5];
+let oddNumbers = arr3.filter(function (element) {
+  return element % 2 !== 0;
+});
 
-console.log("odd numbers in arr1:", oddNumbers.length);
+console.log("odd numbers in arr3:", oddNumbers.length);
