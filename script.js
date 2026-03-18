082c728038d308f117e583c96ed40d25cc53952e
Pallavi Bakale <pallavibakale09@gmail.com>
2026-03-17 22:56:56 -0400

    TD14:JS arrays and functions

--- script.js
+++ script.js
@@ -1,65 +1,15 @@
-let sumNumber = 12;
-let tableNumber = 12;
-let primeNumber = 12;
-let factorNumber = 12;
-let digitSumNumber = 12;
-let armstrongNumber = 153;
+arr1 = [
+  5, 5, 8, 10, 679, 78, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
+];
 
-let sum = 0;
-for (let i = 1; i <= sumNumber; i++) {
-  sum += i;
-}
-console.log("Sum of " + sumNumber + " numbers is: " + sum);
+console.log("greatest number in arr1:", arr1.sort((a, b) => b - a)[0]);
+sum = 0;
+arr1.forEach((element) => {
+  sum += element;
+});
 
-console.log("Table of " + tableNumber);
-for (let i = 1; i <= 10; i++) {
-  console.log(tableNumber + " * " + i + " = " + tableNumber * i);
-}
+console.log("sum of all elements in arr1:", sum);
 
-let isPrime = true;
-if (primeNumber <= 1) {
-  isPrime = false;
-} else {
-  for (let i = 2; i < primeNumber; i++) {
-    if (primeNumber % i === 0) {
-      isPrime = false;
-      break;
-    }
-  }
-}
+let oddNumbers = arr1.filter((element) => element % 2 !== 0);
 
-if (isPrime) {
-  console.log(primeNumber + " is a prime number");
-} else {
-  console.log(primeNumber + " is not a prime number");
-}
-
-console.log("Factors of " + factorNumber);
-for (let i = 1; i <= factorNumber; i++) {
-  if (factorNumber % i === 0) {
-    console.log(i);
-  }
-}
-
-let sumOfDigits = 0;
-let digitTemp = digitSumNumber;
-while (digitTemp > 0) {
-  sumOfDigits += digitTemp % 10;
-  digitTemp = Math.floor(digitTemp / 10);
-}
-console.log("Sum of digits of " + digitSumNumber + " is: " + sumOfDigits);
-
-let sumOfCubes = 0;
-let armstrongTemp = armstrongNumber;
-while (armstrongTemp > 0) {
-  let digit = armstrongTemp % 10;
-  sumOfCubes += digit * digit * digit;
-  armstrongTemp = Math.floor(armstrongTemp / 10);
-}
-
-console.log("Sum of cubes is: " + sumOfCubes);
-if (sumOfCubes === armstrongNumber) {
-  console.log(armstrongNumber + " is an armstrong number");
-} else {
-  console.log(armstrongNumber + " is not an armstrong number");
-}
+console.log("odd numbers in arr1:", oddNumbers.length);
